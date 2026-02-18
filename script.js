d7244c12722e328e656390885838794b20885881
Kishik-K <kishikkhajuria03@gmail.com>
2026-02-18 13:09:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,24 @@
 let habits = JSON.parse(localStorage.getItem("habits")) || [];
 let timerInterval = null;
 let timeLeft = 25 * 60;
+
+
+// ----- ADD HABIT -----
+function addHabit() {
+    const name = document.getElementById("hName").value;
+    const date = document.getElementById("hDate").value;
+    const target = parseInt(document.getElementById("hTarget").value) || 1;
+
+    if (!name || !date) return;
+
+    habits.push({
+        id: Date.now(),
+        name: name,
+        date: date,
+        target: target,
+        current: 0
+    });
+
+    save();
+    document.getElementById("hName").value = "";
+}
